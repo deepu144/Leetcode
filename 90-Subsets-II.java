5dbb7b2fea7beac78d86572dcbc74dda31e1f6e5
Deepu <deepaktmalai65@gmail.com>
2024-07-24 21:20:17 +0530

    [90] [Time Beats: 99.92%] [Memory Beats: 54.07%] - LeetPush

--- 90-Subsets-II.java
+++ 90-Subsets-II.java
@@ -3,12 +3,12 @@ class Solution {
         Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
         subsets(0,arr,arr.length,ans,new ArrayList<>());
-        Set<List<Integer>> ans1 = new HashSet<>(ans);
-        return new ArrayList<>(ans1);
+        return ans;
     }
     public void subsets(int i,int[] arr,int n,List<List<Integer>> ans,List<Integer> subset){
         ans.add(new ArrayList<>(subset));
         for(int j=i;j<n;j++){
+            if(i!=j && arr[j] == arr[j-1]) continue; 
             subset.add(arr[j]);
             subsets(j+1,arr,arr.length,ans,subset);
             subset.remove(subset.size()-1);
