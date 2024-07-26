497d55a549b6140fd0ac97a82e611e39d4f9d183
Deepu <deepaktmalai65@gmail.com>
2024-07-26 22:55:21 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 11.02%] - LeetPush

--- 540-Single-Element-in-a-Sorted-Array.java
+++ 540-Single-Element-in-a-Sorted-Array.java
@@ -1,9 +1,13 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int res=0;
-        for(int i=0;i<nums.length;i++){
-            res^=nums[i];
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            if (mid % 2 == 0 && nums[mid] == nums[mid + 1]) left = mid + 1;
+            else if (mid % 2 == 1 && nums[mid] == nums[mid - 1]) left = mid + 1;
+            else right = mid;
         }
-        return res;
+        return nums[left];
     }
 }
