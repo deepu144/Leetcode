e9f3fc7b7975641cb8b779257565a1e0a875b292
Deepu <deepaktmalai65@gmail.com>
2024-08-01 09:54:51 +0530

    [1283] [Time Beats: 61.32%] [Memory Beats: 72.93%] - LeetPush

--- 1283-Find-the-Smallest-Divisor-Given-a-Threshold.java
+++ 1283-Find-the-Smallest-Divisor-Given-a-Threshold.java
@@ -4,20 +4,14 @@ class Solution {
         for(int n:nums) end=Math.max(end,n);
         while(start<=end){
             int mid=start+(end-start)/2;
-            int t=sum(nums,mid);
-            if(t>threshold){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
+            if(sum(nums,mid)>threshold) start=mid+1;
+            else end=mid-1;          
         }
         return start;
     }
     public int sum(int[] arr,int div){
         int ans=0;
-        for(int n:arr){
-            ans+=(int)Math.ceil((double)n/div);
-        }
+        for(int n:arr) ans+=(int)Math.ceil((double)n/div);
         return ans;
     }
 }
