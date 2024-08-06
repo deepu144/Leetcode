f537a62de995c963ce562d567dbee5fa41619281
Deepu <deepaktmalai65@gmail.com>
2024-08-06 10:45:39 +0530

    [3016] [Time Beats: 46.54%] [Memory Beats: 9.23%] - LeetPush

--- 3016-Minimum-Number-of-Pushes-to-Type-Word-II.java
+++ 3016-Minimum-Number-of-Pushes-to-Type-Word-II.java
@@ -2,17 +2,16 @@ class Solution {
     public int minimumPushes(String word) {
         int[] hash=new int[26];
         for(char c: word.toCharArray()) hash[c-'a']++;
-        Arrays.sort(hash);
-        int res=0,time=1,temp=0;
-        for(int i=25;i>=0;i--){
-            if(hash[i]!=0){
-                if(temp==8){
-                    temp=0;
-                    time++;
-                }
-                res+=(hash[i]*time);
-                temp++;
+        PriorityQueue<Pair<Character,Integer>> pq=new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+        for(int i=0;i<26;i++) if(hash[i]>0) pq.offer(new Pair<>((char)(i+'a'),hash[i]));
+        int res=0,time=1,temp=0;;
+        while(!pq.isEmpty()){
+            if(temp==8){
+                temp=0;
+                time++;
             }
+            res+=(pq.poll().getValue()*time);
+            temp++;
         }
         return res;
     }
