b52db28ab1f4576e40a130830b300b769ad5dcd0
Deepu <deepaktmalai65@gmail.com>
2024-08-09 23:05:44 +0530

    [1754] [Time Beats: 100.00%] [Memory Beats: 23.81%] - LeetPush

--- 1754-Largest-Merge-Of-Two-Strings.java
+++ 1754-Largest-Merge-Of-Two-Strings.java
@@ -1,28 +1,21 @@
 class Solution {
     public String largestMerge(String word1, String word2) {
-        List<Character> li = new ArrayList<>();
+        StringBuilder sb=new StringBuilder();
         char[] x=word1.toCharArray();
         char[] y=word2.toCharArray();
         int n=x.length,m=y.length,a=0,b=0;
         boolean f=false;
         while(a<n && b<m){
-            if(x[a]>y[b]) li.add(x[a++]);
-            else if(x[a]<y[b]) li.add(y[b++]);
+            if(x[a]>y[b]) sb.append(x[a++]);
+            else if(x[a]<y[b]) sb.append(y[b++]);
             else if(x[a]==y[b]){
                 int t = word1.substring(a).compareTo(word2.substring(b));
-                if(t>0) li.add(x[a++]);
-                else li.add(y[b++]);
+                if(t>0) sb.append(x[a++]);
+                else sb.append(y[b++]);
             }
         }
-        StringBuilder sb=new StringBuilder();
-        for(char ch : li) sb.append(ch);
-        if(a==n && b!=m){
-            sb.append(word2.substring(b));
-            return sb.toString();
-        }else if(a!=n && b==m){
-            sb.append(word1.substring(a));
-            return sb.toString();
-        }
+        sb.append(word2.substring(b));
+        sb.append(word1.substring(a));
         return sb.toString();
     }
 }
