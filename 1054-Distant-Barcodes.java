0e28a3565ec7e6d50ed0666c43ddb680e49a0d0b
Deepu <deepaktmalai65@gmail.com>
2024-08-09 21:38:13 +0530

    [1054] [Time Beats: 100.00%] [Memory Beats: 17.02%] - LeetPush

--- 1054-Distant-Barcodes.java
+++ 1054-Distant-Barcodes.java
@@ -1,37 +1,23 @@
 class Solution {
     public int[] rearrangeBarcodes(int[] arr){
-        int n=arr.length,j=0;
-        int[] res=new int[n];
+        int n=arr.length,j=0,max=0;
         int[] hash=new int[10001];
-        for(int i : arr) hash[i]++;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->b.fre-a.fre);
-        for(int i=0;i<10001;i++) if(hash[i]!=0) pq.offer(new Pair(i,hash[i]));
-        while(!pq.isEmpty()){
-            ArrayList<Pair> li=new ArrayList<>();
-            if(j==0){
-                Pair p = pq.poll();
-                res[j++]=p.val;
-                p.fre--;
-                li.add(p);
-            }else{
-                Pair p=null;
-                while(!pq.isEmpty()&&((p=pq.poll()).val==res[j-1])){
-                    li.add(p);
-                }
-                res[j++]=p.val;
-                p.fre--;
-                li.add(p);
-            }
-            for(Pair p : li) if(p.fre!=0) pq.offer(p);
+        for(int i : arr){
+            hash[i]++;
+            if(hash[i]>hash[max]) max=i;
         }
-        return res;
-    }
-}
-class Pair{
-    int val;
-    int fre;
-    Pair(int val,int fre){
-        this.val=val;
-        this.fre=fre;
+        while(hash[max]-->0){
+            arr[j]=max;
+            j+=2;
+        }
+        for(int i=1;i<10001;i++){
+            if(hash[i]==0) continue;
+            while(hash[i]-->0){
+                if(j>=n) j=1;
+                arr[j]=i;
+                j+=2;
+            }
+        }
+        return arr;
     }
 }
