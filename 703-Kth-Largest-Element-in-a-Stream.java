d697cacb062c7c764cc268fded9e3ce25c73ceb6
Deepu <deepaktmalai65@gmail.com>
2024-08-12 06:58:59 +0530

    [703] [Time Beats: 51.46%] [Memory Beats: 33.32%] - LeetPush

---
+++ 703-Kth-Largest-Element-in-a-Stream.java
@@ -0,0 +1,24 @@
+class KthLargest {
+    PriorityQueue<Integer> pq=null;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        pq=new PriorityQueue<>(k);
+        this.k=k;
+        for(int n : nums){
+            if(pq.size()==k && pq.peek()<n) pq.poll();
+            if(pq.size()<k) pq.offer(n);
+        }
+    }
+    
+    public int add(int val) {
+        if(pq.size()==k && pq.peek()<val) pq.poll();
+        if(pq.size()<k) pq.offer(val);
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
