03c5049f24b5512e5a599f1b58c450fcec7616b4
Deepu <deepaktmalai65@gmail.com>
2024-09-11 15:33:57 +0530

    [235] [Time Beats: 100.00%] [Memory Beats: 17.54%] - LeetPush

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.java
@@ -10,29 +10,12 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> pli=new ArrayList<>();
-        findPath(root,p.val,pli);
-        List<TreeNode> qli=new ArrayList<>();
-        findPath(root,q.val,qli);
-        TreeNode ans=null;
-        int i=0,m=pli.size(),n=qli.size();
-        while(m>i || n>i){
-            if(m>i && n>i){
-                if(pli.get(i).val==qli.get(i).val) ans=pli.get(i);
-                else break;
-            }else break;
-            i++;
-        }
-        return ans;
+        return helper(root,p,q);
     }
-    public boolean findPath(TreeNode node,int key,List<TreeNode> li){
-        if(node==null) return false;
-        li.add(node);
-        boolean l = findPath(node.left,key,li);
-        if(node.val==key) return true;
-        boolean r = findPath(node.right,key,li);
-        if(l || r) return true;
-        li.remove(li.size()-1);
-        return false;
+    private TreeNode helper(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null) return root;
+        if(root.val>p.val && root.val>q.val) return helper(root.left, p, q);
+        else if(root.val<p.val && root.val<q.val) return helper(root.right, p, q);
+        else return root;
     }
 }
