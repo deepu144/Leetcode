44a0c584d1dabd991ac7244d557b7559c22ab452
Deepu <deepaktmalai65@gmail.com>
2024-09-19 22:20:33 +0530

    [802] [Time Beats: 32.50%] [Memory Beats: 61.74%] - LeetPush

--- 802-Find-Eventual-Safe-States.java
+++ 802-Find-Eventual-Safe-States.java
@@ -1,23 +1,34 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int V=graph.length;
-        boolean[] path = new boolean[V];
+        List<List<Integer>> adj = new ArrayList<>();
+        int v=graph.length;
+        int[] inDegree = new int[v];
+        for(int i=0;i<v;i++){
+            adj.add(new ArrayList());
+        }
+        for(int i=0;i<v;i++){
+            for(int n : graph[i]){
+                adj.get(n).add(i);
+                inDegree[i]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<v;i++){
+            if(inDegree[i]==0){
+                q.offer(i);
+            }
+        }
         List<Integer> res=new ArrayList<>();
-        for(int i=0;i<V;i++){
-            if(!dfs(i,graph,new boolean[V],path)) res.add(i);
+        int j=0;
+        while(!q.isEmpty()){
+            int n=q.poll();
+            res.add(n);
+            for(int i : adj.get(n)){
+                inDegree[i]--;
+                if(inDegree[i]==0) q.offer(i);
+            }
         }
         Collections.sort(res);
         return res;
-    }
-    public boolean dfs(int v,int[][] adj,boolean[] visit,boolean[] path){
-        visit[v]=true;
-        path[v]=true;
-        for(int i : adj[v]){
-            if(!visit[i]){
-                if(dfs(i,adj,visit,path)) return true;
-            }else if(path[i]) return true;
-        }
-        path[v]=false;
-        return false;
     }
 }
