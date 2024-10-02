f474ea8bfea3ebae284a2031c6db2b58107f3783
Deepu <deepaktmalai65@gmail.com>
2024-10-02 15:39:56 +0530

    [63] [Time Beats: 100.00%] [Memory Beats: 39.28%] - LeetPush

--- 63-Unique-Paths-II.java
+++ 63-Unique-Paths-II.java
@@ -3,13 +3,18 @@ class Solution {
         if(grid[0][0]==1) return 0;
         int m=grid.length,n=grid[0].length;
         int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
-        return generate(grid,m,n,m-1,n-1,dp);
-    }
-    public int generate(int[][] grid,int m,int n,int r,int c,int[][] dp){
-        if(r==0 && c==0) return 1;
-        if(r<0||c<0||grid[r][c]==1) return 0;
-        if(dp[r][c]!=-1) return dp[r][c];
-        return dp[r][c]=generate(grid,m,n,r-1,c,dp)+generate(grid,m,n,r,c-1,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) dp[i][j]=0;
+                else if(j==0 && i==0) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
