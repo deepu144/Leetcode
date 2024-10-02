8753bc80dd1aa944ab0f1907b3e4f20e417b504b
Deepu <deepaktmalai65@gmail.com>
2024-10-02 18:09:42 +0530

    [120] [Time Beats: 99.62%] [Memory Beats: 38.24%] - LeetPush

--- 120-Triangle.java
+++ 120-Triangle.java
@@ -1,17 +1,17 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int m=triangle.size(),n=triangle.get(m-1).size();
-        if(m==1) return triangle.get(0).get(0);
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++) Arrays.fill(dp[i],-1);
-        return generate(triangle,m,0,0,dp);
-    }
-    public int generate(List<List<Integer>> tri,int m,int r,int c,int[][] dp){
-        if(c>=tri.get(r).size()) return (int)(1e9);
-        if(r==m-1) return tri.get(r).get(c);
-        if(dp[r][c]!=-1) return dp[r][c];
-        int a=generate(tri,m,r+1,c,dp)+tri.get(r).get(c);
-        int b=generate(tri,m,r+1,c+1,dp)+tri.get(r).get(c);
-        return dp[r][c]=Math.min(a,b);
+    public int minimumTotal(List<List<Integer>> tri) {
+        int m=tri.size();
+        int[][] dp=new int[m][m];
+        for(int i=0;i<m;i++) dp[m-1][i]=tri.get(m-1).get(i);
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                if(i+1<m){
+                    int a=dp[i+1][j]+tri.get(i).get(j);
+                    int b=dp[i+1][j+1]+tri.get(i).get(j);
+                    dp[i][j]=Math.min(a,b);
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
