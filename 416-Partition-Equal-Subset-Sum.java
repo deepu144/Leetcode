a1d093328a2211f9cebc4a812543faa10f1b7054
Deepu <deepaktmalai65@gmail.com>
2024-10-03 15:11:43 +0530

    [416] [Time Beats: 28.07%] [Memory Beats: 33.04%] - LeetPush

--- 416-Partition-Equal-Subset-Sum.java
+++ 416-Partition-Equal-Subset-Sum.java
@@ -2,14 +2,16 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int sum=0,n=nums.length;
         for(int i=0;i<n;i++) sum+=nums[i];
-        return subset(nums,n-1,sum,0,new Boolean[n][sum+1]);
+        if(sum%2==1) return false;
+        return subset(n,nums,sum/2,n-1,new Boolean[n][sum/2+1]);
     }
-    public boolean subset(int[] nums,int i,int totalSum,int sum,Boolean[][] dp){
-        if(sum==(totalSum-sum)) return true;
-        if(i==-1 || sum>(totalSum-sum)) return false;
+    public boolean subset(int n,int[] arr,int sum,int i,Boolean[][] dp){
+        if(sum==0) return true;
+        if(i==-1) return false;
         if(dp[i][sum]!=null) return dp[i][sum];
-        boolean nt=subset(nums,i-1,totalSum,sum,dp);
-        boolean t=subset(nums,i-1,totalSum,sum+nums[i],dp);
+        boolean nt = subset(n,arr,sum,i-1,dp);
+        boolean t = false;
+        if(sum>=arr[i]) t=subset(n,arr,sum-arr[i],i-1,dp);
         return dp[i][sum]=t||nt;
     }
 }
