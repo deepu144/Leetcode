be65e7074bef3f3b6a478bd95d50c851fe5ea936
Deepu <deepaktmalai65@gmail.com>
2024-10-07 18:20:54 +0530

    [115] [Time Beats: 20.39%] [Memory Beats: 23.49%] - LeetPush

--- 115-Distinct-Subsequences.java
+++ 115-Distinct-Subsequences.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numDistinct(String s1, String s2) {
         int m=s1.length(),n=s2.length();
-        return subsequence(s1,s2,m-1,n-1,new Integer[m+1][n+1]); 
-    }
-    public int subsequence(String s,String t,int i,int j,Integer[][] dp){
-        if(j==-1) return 1;
-        if(i==-1) return 0;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)) dp[i][j] = subsequence(s,t,i-1,j-1,dp) + subsequence(s,t,i-1,j,dp);
-        else dp[i][j] = subsequence(s,t,i-1,j,dp);
-        return dp[i][j];
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<m;i++) dp[i][0]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[m][n];
     }
 }
