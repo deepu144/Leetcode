65cda0755f19e634e5f44f6272776973619310cd
Deepu <deepaktmalai65@gmail.com>
2024-10-14 21:33:20 +0530

    [132] [Time Beats: 84.00%] [Memory Beats: 30.90%] - LeetPush

--- 132-Palindrome-Partitioning-II.java
+++ 132-Palindrome-Partitioning-II.java
@@ -1,25 +1,24 @@
 class Solution {
     public int minCut(String s) {
         int n=s.length();
-        return cut(s,0,n,new Integer[n+1]);
-    }
-    public int cut(String s,int ind,int n,Integer[] dp){
-        if(ind>=n-1) return 0;
-        if(dp[ind]!=null) return dp[ind];
-        int count=0,min=(int)(1e9);
-        for(int i=ind;i<n;i++){
-            if(isPalindrome(s,ind,i)){
-                if(i==n-1) count = 0 + cut(s,i+1,n,dp);
-                else count = 1 + cut(s,i+1,n,dp);
-                min=Math.min(min,count);
+        int[] dp=new int[n+1];
+        dp[n]=-1;
+        boolean[][] palindrome = new boolean[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                if((s.charAt(i)==s.charAt(j)) && (i-j<2 || palindrome[j+1][i-1])) palindrome[j][i]=true;
             }
         }
-        return dp[ind]=min;
-    }
-    public boolean isPalindrome(String str,int a,int b){
-        while(a<b){
-            if(str.charAt(a++)!=str.charAt(b--)) return false;
+        for(int i=n-1;i>=0;i--){
+            int count=0,min=(int)(1e9);
+            for(int j=i;j<n;j++){
+                if(palindrome[i][j]){
+                    count = 1 + dp[j+1];
+                    min=Math.min(min,count);
+                }
+            }
+            dp[i]=min;
         }
-        return true;
+        return dp[0];
     }
 }
