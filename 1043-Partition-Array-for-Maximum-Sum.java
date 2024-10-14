b8b77cf26bda23ce013f9303aa5fd31a9d02d763
Deepu <deepaktmalai65@gmail.com>
2024-10-14 22:06:35 +0530

    [1043] [Time Beats: 99.39%] [Memory Beats: 16.56%] - LeetPush

--- 1043-Partition-Array-for-Maximum-Sum.java
+++ 1043-Partition-Array-for-Maximum-Sum.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n=arr.length;
-        return maxSum(arr,k,n,0,new Integer[n+1]);
+        return maxSum(arr,k,n-1,new Integer[n+1]);
     }
-    public int maxSum(int[] arr,int k,int n,int ind,Integer[] dp){
-        if(ind==n) return 0;
+    public int maxSum(int[] arr,int k,int ind,Integer[] dp){
+        if(ind==-1) return 0;
         if(dp[ind]!=null) return dp[ind];
         int maxi=-1,res=(int)(-1e7);
-        for(int i=ind;i<n;i++){
-            int size=i-ind+1;
+        for(int i=ind;i>=0;i--){
+            int size=ind-i+1;
             if(size>k) break;
             maxi=Math.max(maxi,arr[i]);
-            res=Math.max(res,(maxi*size)+maxSum(arr,k,n,i+1,dp));
+            res=Math.max(res,(maxi*size)+maxSum(arr,k,i-1,dp));
         }
         return dp[ind]=res;
     }
