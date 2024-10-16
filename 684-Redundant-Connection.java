35dccc1d57612c2a0f39c45cc38b8190528b5a14
Deepu <deepaktmalai65@gmail.com>
2024-10-16 22:14:01 +0530

    [684] [Time Beats: 78.59%] [Memory Beats: 75.66%] - LeetPush

---
+++ 684-Redundant-Connection.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int[] res=null;
+        int n=edges.length;
+        DisJointSet ds=new DisJointSet(n+1);
+        for(int[] a : edges){
+            if(!ds.union(a[0],a[1])){
+                res=a;
+            }
+        }
+        return res;
+    }
+}
+class DisJointSet{
+    int[] parent;
+    int[] size;
+    public DisJointSet(int n){
+        parent=new int[n];
+        size=new int[n];
+        Arrays.fill(size,1);
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+    public int find(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=find(parent[u]);
+    }
+    public boolean union(int u,int v){
+        int ul_u=find(u);
+        int ul_v=find(v);
+        if(ul_u==ul_v) return false;
+        if(size[ul_u]>size[ul_v]){
+            parent[ul_v]=ul_u;
+            size[ul_u]+=size[ul_v];
+        }else{
+            parent[ul_u]=ul_v;
+            size[ul_v]+=size[ul_u];
+        }
+        return true;
+    }
+}
