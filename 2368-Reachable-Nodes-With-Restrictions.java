e53c3547825760c996a94db70b9683f598106ffa
Deepu <deepaktmalai65@gmail.com>
2024-10-17 19:14:25 +0530

    [2368] [Time Beats: 98.29%] [Memory Beats: 96.75%] - LeetPush

--- 2368-Reachable-Nodes-With-Restrictions.java
+++ 2368-Reachable-Nodes-With-Restrictions.java
@@ -2,20 +2,37 @@ class Solution {
     public int reachableNodes(int n, int[][] edges, int[] restricted) {
         int[] hash=new int[n];
         for(int i : restricted) hash[i]=1;
-        List<Integer>[] adj=new List[n];
-        for(int i=0;i<n;i++) adj[i]=new ArrayList<>();
+        DisJointSet ds=new DisJointSet(n);
         for(int[] a : edges){
-            adj[a[0]].add(a[1]);
-            adj[a[1]].add(a[0]);
+            if(hash[a[0]]==0 && hash[a[1]]==0) ds.union(a[0],a[1]);
         }
-        return 1+dfs(0,adj,hash,new boolean[n]);
+        return ds.size[ds.find(0)];
     }
-    public int dfs(int v,List<Integer>[] adj,int[] hash,boolean[] visited){
-        visited[v]=true;
-        int val=0;
-        for(int i : adj[v]){
-            if(!visited[i] && hash[i]==0) val += 1+dfs(i,adj,hash,visited);
+}
+class DisJointSet{
+    int[] parent;
+    int[] size;
+    public DisJointSet(int n){
+        parent=new int[n];
+        size=new int[n];
+        for(int i=0;i<n;i++) parent[i]=i;
+        Arrays.fill(size,1);
+    }
+    public int find(int v){
+        if(parent[v]==v) return v;
+        return parent[v]=find(parent[v]);
+    }
+    public boolean union(int u,int v){
+        int ul_u=find(u);
+        int ul_v=find(v);
+        if(ul_v==ul_u) return false;
+        if(size[ul_u]<size[ul_v]){
+            parent[ul_u]=ul_v;
+            size[ul_v]+=size[ul_u];
+        }else{
+            parent[ul_v]=ul_u;
+            size[ul_u]+=size[ul_v];
         }
-        return val;
+        return true;
     }
 }
