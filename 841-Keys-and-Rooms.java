29f6d7053df273b70e2bfdea3927930ac215392d
Deepu <deepaktmalai65@gmail.com>
2024-10-17 15:50:38 +0530

    [841] [Time Beats: 100.00%] [Memory Beats: 37.50%] - LeetPush

--- 841-Keys-and-Rooms.java
+++ 841-Keys-and-Rooms.java
@@ -2,18 +2,14 @@ class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n=rooms.size();
         boolean[] visited=new boolean[n];
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(0);
-        while(!q.isEmpty()){
-            int p=q.poll();
-            visited[p]=true;
-            for(int i : rooms.get(p)){
-                if(!visited[i]){
-                    q.offer(i);
-                }
-            }
-        }
+        dfs(0,rooms,visited);
         for(int i=0;i<n;i++) if(!visited[i]) return false;
         return true;
+    }
+    public void dfs(int room,List<List<Integer>> rooms,boolean[] visited){
+        visited[room]=true;
+        for(int i : rooms.get(room)){
+            if(!visited[i]) dfs(i,rooms,visited);
+        }
     }
 }
