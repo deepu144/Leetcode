6fd9e0abdec9dcab06642f125205f24018ebeeb2
Deepu <deepaktmalai65@gmail.com>
2024-10-31 23:41:32 +0530

    [617] [Time Beats: 11.54%] [Memory Beats: 64.30%] - LeetPush

--- 617-Merge-Two-Binary-Trees.java
+++ 617-Merge-Two-Binary-Trees.java
@@ -28,17 +28,13 @@ class Solution {
                     p[0].left.val+=p[1].left.val;
                     q.offer(new TreeNode[]{p[0].left,p[1].left});
                 }
-            }else{
-                if(p[1].left!=null) p[0].left=p[1].left;
-            }
+            }else if(p[1].left!=null) p[0].left=p[1].left;
             if(p[0].right!=null){
                 if(p[1].right!=null){
                     p[0].right.val+=p[1].right.val;
                     q.offer(new TreeNode[]{p[0].right,p[1].right});
                 }
-            }else{
-                if(p[1].right!=null) p[0].right=p[1].right;
-            }
+            }else if(p[1].right!=null) p[0].right=p[1].right;
         }
         return root1;
     }
