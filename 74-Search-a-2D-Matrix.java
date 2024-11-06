158a7b3fc154fa30cc149cf43d3a03a92be883dd
Deepu <deepaktmalai65@gmail.com>
2024-11-06 07:49:05 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 19.47%] - LeetPush

--- 74-Search-a-2D-Matrix.java
+++ 74-Search-a-2D-Matrix.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m=matrix.length,n=matrix[0].length;
-        int end=m*n-1,start=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(target==matrix[mid/n][mid%n]) return true;
-            else if(target>matrix[mid/n][mid%n]) start=mid+1;
-            else end=mid-1;
+        int m=matrix.length,n=matrix[0].length,row=0,col=n-1;
+        while(row<m && col>=0){
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) row++;
+            else col--;
         }
         return false;
     }
