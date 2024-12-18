de879a28a98ff649622c320167c84d2959328e60
Deepu <deepaktmalai65@gmail.com>
2024-12-18 10:40:01 +0530

    [1475] [Time Beats: 56.44%] [Memory Beats: 49.24%] - LeetPush

--- 1475-Final-Prices-With-a-Special-Discount-in-a-Shop.java
+++ 1475-Final-Prices-With-a-Special-Discount-in-a-Shop.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
-        Stack<int[]> mono=new Stack<>();
+        Stack<Integer> mono=new Stack<>();
         int n=prices.length;
         for(int i=0;i<n;i++){
-            while(!mono.isEmpty() && mono.peek()[0]>=prices[i]){
-                int[] p=mono.pop();
-                prices[p[1]]=p[0]-prices[i];
+            while(!mono.isEmpty() && prices[mono.peek()]>=prices[i]){
+                int p=mono.pop();
+                prices[p]-=prices[i];
             }
-            mono.push(new int[]{prices[i],i});
+            mono.push(i);
         }
         return prices;
     }
