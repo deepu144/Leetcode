7f49c369e182d1a3974dcfdcdc7ec9d89147f9c4
Deepu <deepaktmalai65@gmail.com>
2024-12-20 10:59:15 +0530

    Time: 1 ms (72.88%), Space: 48.6 MB (43.36%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -15,44 +15,17 @@
  */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        if(root==null) return null;
-        List<List<Integer>> tree=new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        boolean isOdd=false;
-        while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> li=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode p=q.poll();
-                li.add(p.val);
-                if(p.left!=null){
-                    q.add(p.left);
-                    q.add(p.right);
-                }
-            }
-            if(isOdd) Collections.reverse(li);
-            tree.add(li);
-            isOdd=!isOdd;
+        dfs(root.left,root.right,1);
+        return root;
+    }
+    public void dfs(TreeNode left,TreeNode right,int depth){
+        if(left==null) return;
+        if((depth & 1)==1){
+            int t=left.val;
+            left.val=right.val;
+            right.val=t;
         }
-        TreeNode res=new TreeNode(tree.getFirst().getFirst());
-        q.clear();
-        q.add(res);
-        int ind=1;
-        while(!q.isEmpty()){
-            int size=q.size();
-            if(ind==tree.size()) break;
-            List<Integer> li=tree.get(ind);
-            int j=0;
-            for(int i=0;i<size;i++){
-                TreeNode t=q.poll();
-                t.left=new TreeNode(li.get(j++));
-                t.right=new TreeNode(li.get(j++));
-                q.add(t.left);
-                q.add(t.right);
-            }
-            ind++;
-        }
-        return res;
+        dfs(left.left,right.right,depth+1);
+        dfs(left.right,right.left,depth+1);
     }
 }
