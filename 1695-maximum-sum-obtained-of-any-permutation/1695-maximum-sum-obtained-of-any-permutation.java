2cfa13f0324d2694c53890d752e16918af3fa2c4
Deepu <deepaktmalai65@gmail.com>
2024-12-20 23:20:08 +0530

    Time: 24 ms (93.25%), Space: 79.3 MB (48.47%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.java
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.java
@@ -2,16 +2,16 @@ class Solution {
     final int MOD=(int)(1e9+7);
     public int maxSumRangeQuery(int[] nums, int[][] requests) {
         final int n=nums.length;
-        int[] sweep=new int[n];
+        int[] sweep=new int[n+1];
         for(int[] a : requests){
             sweep[a[0]]++;
-            if(a[1]+1<n) sweep[a[1]+1]--;
+            sweep[a[1]+1]--;
         }
-        for(int i=1;i<n;i++) sweep[i]+=sweep[i-1];
+        for(int i=1;i<=n;i++) sweep[i]+=sweep[i-1];
         Arrays.sort(nums);
         Arrays.sort(sweep);
         long res=0;
-        for(int i=0;i<n;i++) res+=(long)nums[i]*sweep[i];
+        for(int i=0;i<n;i++) res+=(long)nums[i]*sweep[i+1];
         return (int)(res%MOD);
     }
 }
