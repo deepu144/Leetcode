afedee29d585ac3385eab49f73cec7cf12041a4b
Deepu <deepaktmalai65@gmail.com>
2024-12-24 15:23:18 +0530

    Time: 76 ms (99.21%), Space: 131 MB (28.35%) - LeetHub

--- 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.java
+++ 3439-find-minimum-diameter-after-merging-two-trees/3439-find-minimum-diameter-after-merging-two-trees.java
@@ -1,41 +1,36 @@
 class Solution {
     final String COMMA=",";
+    int tree1=0,tree2=0;
     public int minimumDiameterAfterMerge(int[][] edges1, int[][] edges2) {
+        if(edges1.length==0 && edges2.length==0) return 1;
         List<Integer>[] adj1=getAdjacentList(edges1);
         List<Integer>[] adj2=getAdjacentList(edges2);
-        int max1=adj1.length,max2=adj2.length,tree1=0,tree2=0;
-        // Integer[][] dp1= new Integer[max1][max1+1];
-        // Integer[][] dp2= new Integer[max2][max2+1];
-        HashMap<String,Integer> dp1=new HashMap<>();
-        HashMap<String,Integer> dp2=new HashMap<>();
-        for(int i=0;i<max1;i++){
-            if(adj1[i].size()==1){
-                tree1=Math.max(tree1,dfs(adj1,i,-1,dp1));
-            }
-        }
-        for(int i=0;i<max2;i++){
-            if(adj2[i].size()==1){
-                tree2=Math.max(tree2,dfs(adj2,i,-1,dp2));
-            }
-        }
+        int max1=adj1.length,max2=adj2.length;
+        if(edges1.length!=0) diameter(adj1,0,-1,true);
+        if(edges2.length!=0) diameter(adj2,0,-1,false);
         int t1= tree1>=2?tree1%2==0?(tree1/2):(tree1/2)+1:tree1;
         int t2= tree2>=2?tree2%2==0?(tree2/2):(tree2/2)+1:tree2;
         if(t1+t2+1<tree1)return tree1;
         if(t1+t2+1<tree2) return tree2;
         return t1+t2+1;
     }
-    public int dfs(List<Integer>[] adj,int node,int parent,HashMap<String,Integer> dp){
+    public int diameter(List<Integer>[] adj,int node,int parent,boolean isFirstTree){
         if(adj[node].size()==1 && adj[node].getFirst()==parent){
             return 0;
         }
-        String s=node+COMMA+parent;
-        if(dp.containsKey(s)) return dp.get(s);
-        int max=-1;
+        int max1=0,max2=0;
         for(int n : adj[node]){
-            if(n!=parent) max=Math.max(max,1+dfs(adj,n,node,dp));
+            if(n!=parent){
+                int t=1+diameter(adj,n,node,isFirstTree);
+                if(t>max1){
+                    max2=max1;
+                    max1=t;
+                }else if(t>max2) max2=t;
+            }
         }
-        dp.put(s,max);
-        return max;
+        if(isFirstTree) tree1= Math.max(tree1,max1+max2);
+        else tree2=Math.max(max1+max2,tree2);
+        return max1;
     }
     public List<Integer>[] getAdjacentList(int[][] edges){
         int max=-1;
