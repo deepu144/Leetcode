e96ee945f9fc968c47d526b5d7f9b3d0705bb0da
Deepu <deepaktmalai65@gmail.com>
2024-12-27 21:00:29 +0530

    Time: 0 ms (100%), Space: 40.7 MB (22.97%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.java
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.java
@@ -1,38 +1,14 @@
 class Solution {
     public int minOperations(int n) {
-        if(n==9078) return 5;
-        if(n==53723||n==97123) return 7;
-        if(n==14026) return 6;
-        String binary=Integer.toString(n,2);
-        int len=binary.length(),ones=Integer.bitCount(n);
-        StringBuilder sb=new StringBuilder(binary);
-        sb.insert(0,'0');
-        int unSet=0,consecutiveOnes=0,extra=0;
-        for(int i=len;i>=0;i--){
-            if(sb.charAt(i)=='0'){
-                unSet+=(Math.pow(2,len-i)); 
-                if(consecutiveOnes>1){
-                    extra++;
-                    sb.setCharAt(i,'1');
-                    for(int j=i+1;j<=i+consecutiveOnes;j++){
-                        sb.setCharAt(j,'0');
-                    }
-                }
-                consecutiveOnes=0;
-            }
-            else consecutiveOnes++;
+        int count=0;
+        while(n>0){
+            int x=(int)(Math.log(n)/Math.log(2)),prev=(int)Math.pow(2,x),next=(int)Math.pow(2,x+1);
+            int diff1=n-prev;
+            int diff2=next-n;
+            if(diff1>diff2) n=diff2;
+            else n=diff1;
+            count++;
         }
-        if(sb.charAt(0)=='0') sb.deleteCharAt(0);
-        String temp=sb.toString();
-        int p=Integer.parseInt(temp,2);
-        return Math.min(extra+operation(p,temp,len,Integer.bitCount(p)),operation(n,binary,len,ones));
-    }
-    public int operation(int n,String binary,int len,int ones){
-        int val=0;
-        for(int i=0;i<len;i++){
-            if(binary.charAt(i)=='0') val+=(Math.pow(2,len-i-1));
-        }
-        val++;
-        return Math.min(ones,Integer.bitCount(val)+1);
+        return count;
     }
 }
