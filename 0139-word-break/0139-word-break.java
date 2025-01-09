f9d4342ca83542e8c3d99571e81d57675c082bd2
Deepu <deepaktmalai65@gmail.com>
2025-01-09 15:33:55 +0530

    Time: 1 ms (99.52%), Space: 42.1 MB (83.89%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,18 +1,40 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        HashMap<String,Integer> map=new HashMap<>();
-        for(String t : wordDict) map.put(t,1);
+        TrieNode root=new TrieNode();
         int n=s.length();
-        return partition(s,n,0,map,new Boolean[n+1]);
+        for(String t : wordDict) insert(t,root);
+        return helper(s,n,root,0,new Boolean[n]);
     }
-    public boolean partition(String s,int n,int ind,HashMap<String,Integer> map,Boolean[] dp){
-        if(ind==n) return true;
-        if(dp[ind]!=null) return dp[ind];
-        for(int i=ind;i<n;i++){
-            if(map.containsKey(s.substring(ind,i+1))){
-                if(partition(s,n,i+1,map,dp)) return dp[ind]=true;
+    public boolean helper(String s,int n,TrieNode root,int i,Boolean[] dp){
+        if(i==n) return true;
+        if(dp[i]!=null) return dp[i];
+        TrieNode node=root;
+        for(int j=i;j<n;j++){
+            int ind=s.charAt(j)-'a';
+            if(node.child[ind]==null) return dp[i]=false;
+            node=node.child[ind];
+            if(node.isLeaf){
+                if(helper(s,n,root,j+1,dp)) return dp[i]=true;
             }
         }
-        return dp[ind]=false;
+        return dp[i]=false;
+    }
+    public void insert(String word,TrieNode root){
+        TrieNode node=root;
+        int n=word.length();
+        for(int i=0;i<n;i++){
+            int ind=word.charAt(i)-'a';
+            if(node.child[ind]==null) node.child[ind]=new TrieNode();
+            node=node.child[ind];
+        }
+        node.isLeaf=true;
+    }
+}
+class TrieNode{
+    boolean isLeaf;
+    TrieNode[] child;
+    public TrieNode(){
+        isLeaf=false;
+        child=new TrieNode[26];
     }
 }
