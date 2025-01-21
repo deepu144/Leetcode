f2a5fec96340de07e572fdfd103feb7308b7deff
Deepu <deepaktmalai65@gmail.com>
2025-01-21 13:55:58 +0530

    Time: 6 ms (33.68%), Space: 58 MB (14.51%) - LeetHub

--- 2145-grid-game/2145-grid-game.java
+++ 2145-grid-game/2145-grid-game.java
@@ -1,18 +1,15 @@
 class Solution {
     public long gridGame(int[][] grid) {
         int n=grid[0].length;
-        long[] prefixZero=new long[n];
+        long[] suffixZero=new long[n+1];
         long[] prefixOne=new long[n+1];
-        long sum1=0,sum2=0;
-        for(int i=0;i<n;i++){
-            sum1+=grid[0][i];
-            prefixZero[i]=sum1;
-            sum2+=grid[1][i];
-            prefixOne[i+1]=sum2;
+        for(int i=1;i<=n;i++){
+            prefixOne[i]=prefixOne[i-1]+grid[1][i-1];
+            suffixZero[n-i]=suffixZero[n-i+1]+grid[0][n-i];          
         }
         long max=Long.MAX_VALUE;
         for(int i=0;i<n;i++){
-            long leftZero=prefixZero[n-1]-prefixZero[i];
+            long leftZero=suffixZero[i+1];
             long rightOne=prefixOne[i];
             max=Math.min(max,Math.max(leftZero,rightOne));
         }
