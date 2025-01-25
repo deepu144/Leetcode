72e9d1423170f79b848bdffa9e86fefa97539f9c
Deepu <deepaktmalai65@gmail.com>
2025-01-25 19:15:37 +0530

    Time: 15 ms (95.28%), Space: 174.1 MB (42.78%) - LeetHub

---
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        DisJointSet ds=new DisJointSet(n);
+        for(int[] a : edges) ds.union(a[0],a[1]);
+        return ds.find(source)==ds.find(destination);
+    }
+}
+class DisJointSet{
+    int[] parent;
+    int[] size;
+    public DisJointSet(int n){
+        parent=new int[n];
+        size=new int[n];
+        Arrays.fill(size,1);
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+    public int find(int u){
+        if(parent[u]==u) return u;
+        return parent[u]=find(parent[u]);
+    }
+    public boolean union(int u,int v){
+        int ul_u=find(u);
+        int ul_v=find(v);
+        if(ul_u==ul_v) return false;
+        if(size[ul_u]>size[ul_v]){
+            parent[ul_v]=ul_u;
+            size[ul_u]+=size[ul_v];
+        }else{
+            parent[ul_u]=ul_v;
+            size[ul_v]+=size[ul_u];
+        }
+        return true;
+    }
+}
