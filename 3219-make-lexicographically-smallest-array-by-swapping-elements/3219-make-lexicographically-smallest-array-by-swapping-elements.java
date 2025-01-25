27d3e04bf21f0b182119d279991dba95d893b40d
Deepu <deepaktmalai65@gmail.com>
2025-01-25 15:18:03 +0530

    Time: 138 ms (21.21%), Space: 66.4 MB (66.67%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,30 +1,19 @@
 class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        int n=nums.length,index=0;
+        int n=nums.length;
         int[][] arr=new int[n][2];
-        int[] res=new int[n];
-        List<PriorityQueue<Integer>> pq=new ArrayList<>();
-        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++) arr[i]=new int[]{nums[i],i};
         Arrays.sort(arr,(a,b)->a[0]-b[0]);
-        int maxLimit=nums[0]+limit;
-        for(int i=0;i<n;i++){
-            if(maxLimit>=arr[i][0]){
-                if(index==pq.size()) pq.add(new PriorityQueue<>());
-                pq.get(index).add(arr[i][0]);
-                map.put(arr[i][1],index);
-            }else{
-                index++;
-                if(index==pq.size()) pq.add(new PriorityQueue<>());
-                pq.get(index).add(arr[i][0]);
-                map.put(arr[i][1],index);
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<n-1;i++){
+            int j=i;
+            pq.add(arr[i][1]);
+            while(i<n-1 && arr[i+1][0]-arr[i][0]<=limit){
+                pq.offer(arr[i+1][1]);
+                i++;
             }
-            maxLimit=arr[i][0]+limit;
+            for(int k=j;k<=i;k++) nums[pq.poll()]=arr[k][0];
         }
-        for(int i=0;i<n;i++){
-            index=map.get(i);
-            res[i]=pq.get(index).poll();
-        }
-        return res;
+        return nums;
     }
 }
