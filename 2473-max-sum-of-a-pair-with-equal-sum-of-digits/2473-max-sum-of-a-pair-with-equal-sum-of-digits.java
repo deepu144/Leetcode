cb9c82a9d3000a841546d38425308f3ce5001e8d
Deepu <deepaktmalai65@gmail.com>
2025-02-12 11:24:25 +0530

    Time: 21 ms (92.09%), Space: 55.4 MB (92.09%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,28 +1,11 @@
 class Solution {
     public int maximumSum(int[] nums) {
-        HashMap<Integer,int[]> map=new HashMap<>();
+        int[] map=new int[82];
         int n=nums.length,max=-1;
         for(int i=0;i<n;i++){
-            int sumOfDigits=sumOfDigits(nums[i]);
-            if(map.containsKey(sumOfDigits)){
-                int[] p=map.get(sumOfDigits);
-                if(p[0]<=nums[i]){
-                    p[1]=p[0];
-                    p[0]=nums[i];
-                }else if(p[1]==-1 || p[1]<nums[i]) p[1]=nums[i];
-            }else{
-                int[] p=new int[]{-1,-1};
-                p[0]=nums[i];
-                map.put(sumOfDigits,p);
-            }
-        }
-        for(int key : map.keySet()){
-            int[] p=map.get(key);
-            int sum=0;
-            if(p[0]!=-1 && p[1]!=-1){
-                sum=p[0]+p[1];
-                max=Math.max(max,sum);
-            }
+            int sumOfDigits=sumOfDigits(nums[i]),a=map[sumOfDigits];
+            if(a!=0) max=Math.max(max,a+nums[i]);
+            map[sumOfDigits]=Math.max(a,nums[i]);
         }
         return max;
     }
