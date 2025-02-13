1d22218e1ad58758214a0b51ee764a57843f15c6
Deepu <deepaktmalai65@gmail.com>
2025-02-13 18:15:43 +0530

    Time: 47 ms (97.45%), Space: 109.4 MB (72.61%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.java
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.java
@@ -1,9 +1,8 @@
 class Solution {
     public int largestPathValue(String colors, int[][] edges) {
-        int n=colors.length(),lastNode=-1,max=1;
+        int n=colors.length(),visited=0,max=1;
         int[] inDegree=new int[n];
         int[][] dp=new int[n][26];
-        Set<Integer> set=new HashSet<>();
         List<Integer>[] adj=new List[n];
         for(int  i=0;i<n;i++) adj[i]=new ArrayList<>();
         for(int[] edge : edges){
@@ -15,34 +14,22 @@ class Solution {
             if(inDegree[i]==0){
                 q.offer(i);
                 dp[i][colors.charAt(i)-'a']++;
-                set.add(i);
             }
         }
         while(!q.isEmpty()){
             int node=q.poll();
+            visited++;
             for(int neighbour : adj[node]){
-                if(set.contains(neighbour)){
-                    for(int i=0;i<26;i++){
-                        dp[node][i]=Math.max(dp[node][i],dp[neighbour][i]);
-                        max=Math.max(dp[node][i],max);
-                    }
-                }else{
-                    inDegree[neighbour]--;
-                    for(int i=0;i<26;i++){
-                        dp[neighbour][i]=Math.max(dp[neighbour][i],dp[node][i]);
-                        max=Math.max(dp[node][i],max);
-                    }
-                    if(inDegree[neighbour]==0){
-                        q.offer(neighbour);
-                        char ch=colors.charAt(neighbour);
-                        dp[neighbour][ch-'a']++;
-                        max=Math.max(dp[neighbour][ch-'a'],max);
-                        set.add(neighbour);
-                    }
+                inDegree[neighbour]--;
+                for(int i=0;i<26;i++) dp[neighbour][i]=Math.max(dp[neighbour][i],dp[node][i]);
+                if(inDegree[neighbour]==0){
+                    q.offer(neighbour);
+                    char ch=colors.charAt(neighbour);
+                    dp[neighbour][ch-'a']++;
+                    max=Math.max(dp[neighbour][ch-'a'],max);
                 }
             }
         }
-        if(set.size()!=n) return -1;
-        return max;
+        return visited!=n?-1:max;
     }
 }
