0e07acf55a253eb6bf7858422bcb8aa00abb77b8
Deepu <deepaktmalai65@gmail.com>
2025-02-19 13:47:46 +0530

    Time: 2 ms (83.83%), Space: 42.3 MB (70.84%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,6 +1,13 @@
 class Solution {
     final static char[] ch={'a','b','c'};
-    public void generate(int i,int n,StringBuilder sb,List<String> li){
+    public String getHappyString(int n, int k){
+        List<String> ans=new ArrayList<>();
+        generate(0,n,new StringBuilder(),ans,k);
+        if(ans.size()<k) return "";
+        return ans.getLast();
+    }
+    public void generate(int i,int n,StringBuilder sb,List<String> li,int k){
+        if(li.size()==k) return;
         if(i==n){
             li.add(sb.toString());
             return;
@@ -8,16 +15,9 @@ class Solution {
         for(int j=0;j<3;j++){
             if(i==0||sb.charAt(i-1)!=ch[j]){
                 sb.append(ch[j]);
-                generate(i+1,n,sb,li);
+                generate(i+1,n,sb,li,k);
                 sb.deleteCharAt(sb.length()-1);
             }
         }
-    }
-    public String getHappyString(int n, int k) {
-        List<String> ans=new ArrayList<>();
-        generate(0,n,new StringBuilder(),ans);
-        System.out.println(ans);
-        if(ans.size()<k) return "";
-        return ans.get(k-1);
     }
 }
