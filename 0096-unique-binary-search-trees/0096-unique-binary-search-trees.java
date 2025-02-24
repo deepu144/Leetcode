c2a6f11857bcbdfda4977ea28da65b628a239f32
Deepu <deepaktmalai65@gmail.com>
2025-02-24 14:07:50 +0530

    Time: 0 ms (100%), Space: 40.3 MB (78.97%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,10 +1,7 @@
 class Solution {
     public int numTrees(int n) {
         double res=1,a=n*2;
-        for(int i=n;i>=2;i--){
-            res*=a/i;
-            a--;
-        }
+        for(int i=n;i>=2;i--,a--) res*=a/i;
         return (int)Math.round(res);
     }
 }
