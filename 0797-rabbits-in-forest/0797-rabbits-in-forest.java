91b16d187df9646b2aaffae7748d0fbe40d74c63
Deepu <deepaktmalai65@gmail.com>
2025-04-20 12:55:20 +0530

    Time: 1 ms (93.98%), Space: 42.6 MB (31.57%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -4,16 +4,12 @@ class Solution {
         int n=answers.length,count=0;
         for(int val : answers) hash[val]++;
         count+=hash[0];
-        for(int i=2;i<1001;i++){
+        for(int i=1;i<1001;i++){
             if(hash[i]==0) continue;
-            if(hash[i]<=i+1){
-                count+=(i+1);
-                continue;
-            }
             int q=hash[i]/(i+1),rem=hash[i]%(i+1);
             count+=(q*(i+1));
             if(rem!=0) count+=(i+1);
         }
-        return count+(hash[1]%2==0?hash[1]:hash[1]+1);
+        return count;
     }
 }
