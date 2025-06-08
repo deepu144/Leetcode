c06e3bf2bf3baec3d280dfad949e16c83f31965d
Deepu <deepaktmalai65@gmail.com>
2025-06-08 19:41:38 +0530

    Time: 1 ms (100%), Space: 48.6 MB (39.08%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,15 +1,15 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> res=new ArrayList<>();
-        for(int i=1;i<=9;i++) dfs(n,i,res);
-        return res;
+        List<Integer> li=new ArrayList<>();
+        helper(-1,n,li);
+        return li;
     }
-    public void dfs(int n,int val,List<Integer> res){
-        if(val>n) return;
-        res.add(val);
-        for(int i=0;i<=9;i++){
-            if(val*10+i>n) break;
-            dfs(n,val*10+i,res);
+    public void helper(int n,int limit,List<Integer> li){
+        for(int j=(n==-1?1:0);j<10;j++){
+            int val = n==-1?j:(n*10)+j;
+            if(val>limit) break;
+            li.add(val);
+            helper(val,limit,li);
         }
     }
 }
