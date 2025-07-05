92ed2705e49efd33efb3af0d43fee3647fd663cd
Deepu <deepaktmalai65@gmail.com>
2025-07-05 23:22:25 +0530

    Time: 69 ms (100%), Space: 94.5 MB (100%) - LeetHub

---
+++ 3927-minimum-cost-path-with-alternating-directions-ii/3927-minimum-cost-path-with-alternating-directions-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    final int[][] dir={{0,1},{1,0}};
+    public long minCost(int m, int n, int[][] waitCost) {
+        return helper(m,n,waitCost,0,0,new Long[m][n]);
+    }
+    public long helper(int r,int c,int[][] waitCost,int i,int j,Long[][] dp){
+        if(i==r-1 && j==c-1) return 1L-waitCost[0][0];
+        long min=Long.MAX_VALUE;
+        if(dp[i][j]!=null) return dp[i][j];
+        long cost = waitCost[i][j];
+        for(int[] d : dir){
+            int x=d[0]+i;
+            int y=d[1]+j;
+            if(x>=r||y>=c) continue;
+            long res = cost + (x+1)*(y+1) + helper(r,c,waitCost,x,y,dp);
+            min = Math.min(min,res);
+        }
+        return dp[i][j]=min;
+    }
+}
