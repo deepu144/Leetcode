6b83a3569b6caf31cbc4cef324a6b459c7b5df45
Deepu <deepaktmalai65@gmail.com>
2025-07-07 10:50:15 +0530

    Time: 74 ms (19%), Space: 76.3 MB (12.12%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -2,15 +2,15 @@ class Solution {
     public int maxEvents(int[][] events) {
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         Arrays.sort(events,(a,b)->a[0]-b[0]);
-        int i=0,res=0,n=events.length;
+        int i=0,n=events.length,cnt=0;
         for(int day=1;day<=100000;day++){
-            while(!pq.isEmpty() && pq.peek()<day) pq.poll();
+            while(!pq.isEmpty() && day>pq.peek()) pq.poll();
             while(i<n && day==events[i][0]) pq.offer(events[i++][1]);
             if(!pq.isEmpty()){
-                res++;
                 pq.poll();
+                cnt++;
             }
         }
-        return res;
+        return cnt;
     }
 }
