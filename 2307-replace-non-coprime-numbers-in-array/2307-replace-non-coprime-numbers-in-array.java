29396782baeaf2f849433e6ce893d3fde77b3171
Deepu <deepaktmalai65@gmail.com>
2025-09-16 20:32:09 +0530

    Time: 34 ms (50.52%), Space: 61.1 MB (84.54%) - LeetHub

---
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        int n = nums.length,i=0;
+        List<Integer> ans = new ArrayList<>();
+        List<Long> temp = new LinkedList<>();
+        while(i<n){
+            long mul = nums[i];
+            while(i+1<n){
+                long gcd = gcd(mul,nums[i+1]);
+                if(gcd>1L){
+                    mul = (mul * nums[i+1])/gcd;
+                    i++;
+                }else break;
+            }
+            if(!temp.isEmpty()){
+                boolean flag = false;
+                while(!temp.isEmpty()){
+                    long prev = temp.getLast();
+                    long gcd = gcd(prev,mul);
+                    if(gcd != 1) {
+                        mul = (mul * prev) / gcd;
+                        temp.removeLast();
+                    }else break;
+                }
+                temp.addLast(mul);
+            }else temp.addLast(mul);
+            
+            i++;
+        }
+        for(long val : temp) ans.add((int)val);
+        return ans;
+    }
+    public long gcd(long a, long b) {
+        return b==0 ? a : gcd(b, a % b);
+    }
+}
