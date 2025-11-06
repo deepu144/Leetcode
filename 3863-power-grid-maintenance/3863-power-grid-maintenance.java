0fbd8be8c73f3454ac3a1457c563a8e89de9d022
Deepu <deepaktmalai65@gmail.com>
2025-11-06 16:51:54 +0530

    Time: 72 ms (88.7%), Space: 253 MB (8.37%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.java
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.java
@@ -1,66 +1,73 @@
 class Solution {
     public int[] processQueries(int c, int[][] connections, int[][] queries) {
-        int components=0,temp=0;
-        DisJointSet ds=new DisJointSet(c+1);
-        HashMap<Integer,Integer> map=new HashMap<>();
-        boolean[] isOffline=new boolean[c+1];
-        for(int[] a : connections) ds.union(a[0],a[1]);
+        DisJointSet ds = new DisJointSet(c+1);
+        int components = 0, m=0, j=0;
+        for(int[] q : queries) if(q[0]==1) m++;
+        boolean[] isOnline = new boolean[c+1];
+        int[] res = new int[m];
+        for(int[] connection : connections) ds.union(connection[0],connection[1]);
         for(int i=1;i<=c;i++){
-            int ul_u = ds.find(i);
-            if(ul_u==i) components++;
-            if(!map.containsKey(ul_u)) map.put(ul_u,temp++);
+            if(ds.parent[i]==i) components++;
+            isOnline[i]=true;
         }
-        TreeSet[] set=new TreeSet[components];
-        for(int i=0;i<components;i++) set[i]=new TreeSet();
+        PriorityQueue<Integer>[] pq = new PriorityQueue[components];
+        for(int i=0;i<components;i++) pq[i] = new PriorityQueue<>();
+        Map<Integer,Integer> pqIndex = new HashMap<>();
         for(int i=1;i<=c;i++){
-            int ul_u = ds.find(i);
-            int index = map.get(ul_u);
-            set[index].add(i);
+            int ul_i = ds.find(i);
+            int idx = pqIndex.getOrDefault(ul_i,-1);
+            if(idx == -1) {
+                idx = j++;
+                pqIndex.put(ul_i,idx);
+            }
+            pq[idx].offer(i);
         }
-        temp=0;
-        for(int[] a : queries) if(a[0]==1) temp++;
-        int[] res=new int[temp];
-        temp=0;
-        for(int[] a : queries){
-            int index = map.get(ds.find(a[1]));
-            if(a[0]==1){
-                if(set[index].isEmpty()) res[temp]=-1;
-                else{
-                    if(isOffline[a[1]]) res[temp]=(int)set[index].getFirst();
-                    else res[temp]=a[1];
+        j=0;
+        for(int[] query : queries){
+            if(query[0]==2) isOnline[query[1]]=false;
+            else{
+                int ans = query[1];
+                if(!isOnline[query[1]]){
+                    int idx = pqIndex.get(ds.find(query[1]));
+                    while(!pq[idx].isEmpty() && !isOnline[pq[idx].peek()]) pq[idx].poll();
+                    if(pq[idx].isEmpty()) ans = -1;
+                    else ans = pq[idx].peek();
                 }
-                temp++;
-            }else{
-                set[index].remove(a[1]);
-                isOffline[a[1]]=true;
+                res[j++] = ans;
             }
         }
         return res;
     }
 }
+
 class DisJointSet{
-    int[] parent;
+    int n;
     int[] size;
+    int[] parent;
     public DisJointSet(int n){
-        parent=new int[n];
-        size=new int[n];
-        Arrays.fill(size,1);
-        for(int i=0;i<n;i++) parent[i]=i;
+        this.n = n;
+        this.size = new int[n];
+        this.parent = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
     }
     public int find(int u){
         if(parent[u]==u) return u;
-        return parent[u]=find(parent[u]);
+        return find(parent[u]);
     }
     public boolean union(int u,int v){
-        int ul_u=find(u);
-        int ul_v=find(v);
-        if(ul_u==ul_v) return false;
-        if(size[ul_u]>size[ul_v]){
-            parent[ul_v]=ul_u;
-            size[ul_u]+=size[ul_v];
+        int ul_u = find(u);
+        int ul_v = find(v);
+        if(ul_u == ul_v) return false;
+
+        if(size[ul_u] > size[ul_v]){
+            parent[ul_v] = ul_u;
+            size[ul_u] += size[ul_v];
         }else{
-            parent[ul_u]=ul_v;
-            size[ul_v]+=size[ul_u];
+            parent[ul_u] = ul_v;
+            size[ul_v] += size[ul_u];
         }
         return true;
     }
