ecfeb4e3b43625ff9c816e420121acf21433655c
Deepu <deepaktmalai65@gmail.com>
2026-01-20 22:21:04 +0530

    Time: 3 ms (57.02%), Space: 47 MB (11.4%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.java
@@ -6,7 +6,7 @@ class Solution {
             int val = nums.get(i);
             if(val == 2) ans[i] = -1;
             else{
-                for(int j=1;j<=val;j++){
+                for(int j=1;j<val;j++){
                     if((j|(j+1)) == val){
                         ans[i] = j;
                         break;
